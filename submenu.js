f66d1e39f9ade8af102a5cef37e0fff27d25691b
RubenHernandez2022 <ben_arg@hotmail.com>
2022-11-30 21:55:42 -0300

    proyecto

--- submenu.js
+++ submenu.js
@@ -13,13 +13,27 @@
                                 });
                                       });
 
+// let nombre="";             
+// let respuesta="";
 // function saludo(){
-//     let nombre=prompt("Ingrese nombre:");
-//     document.getElementById("resultado").innerHTML="Bienvenida/o:  "+nombre;
+//     nombre=localStorage.getItem("guardar",JSON.stringify(nombre))
+//     respuesta=prompt("desea ingresar como: "+nombre+"SI O NO");
+//     console.log(respuesta);
+//     console.log(nombre)
+//     if (respuesta=="NO"){
+//         nombre=prompt("Ingrese nombre:");
+//         document.getElementById("resultado").innerHTML="Bienvenida/o:  "+nombre;
+//         localStorage.setItem("guardar",JSON.stringify(nombre))
+//         nombre=localStorage.getItem("guardar")
+//         }else
+//      { 
+//         document.getElementById("resultado").innerHTML="Bienvenida/o:  "+nombre;
+//        }
+//      }
     
-// }
-
-//  saludo();
+ 
+ 
+//   saludo();
 
  function cambiar(){
      document.getElementById("direccion").innerHTML="Estamos en Chiclana 490, BHI";
